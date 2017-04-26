b82ac47b62fd6985cfcb0ee566168a37ba96c4b2
Brett Celestre <bcelestre@gmail.com>
2017-04-26 15:21:59 -0700

    Sets up BookList container and sets up react-redux

---
+++ src/reducers/reducers_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascipt: The Good Parts' },
+    { title: 'Lord of the Rings' },
+    { title: 'The Count of Monte Cristo' },
+    { title: 'Eloquent JavaScript' }
+  ]
+}
